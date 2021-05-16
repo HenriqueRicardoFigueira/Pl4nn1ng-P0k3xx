b85800a4eab59bc6aa1a6256a3fa54bc67854816
HenriqueRicardoFigueira <henrislip@gmail.com>
2021-05-16 17:26:13 -0300

    add projeto com signal2

---
+++ ch4t/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ch4t.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
