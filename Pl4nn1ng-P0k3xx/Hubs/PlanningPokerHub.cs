664c7463474e562e82744b1d6cafa1bf99c93ec9
HenriqueRicardoFigueira <henrislip@gmail.com>
2021-05-16 19:37:21 -0300

    create project with react , configure signal and add initial hub

---
+++ Pl4nn1ng-P0k3xx/Hubs/PlanningPokerHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Pl4nn1ng_P0k3xx.Hubs
+{
+    public class PlanningPokerHub : Hub
+    {
+        public async override Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            await Clients.Caller.SendAsync("Message", "Conexão criada com sucesso!");
+        }
+
+        public async Task SubscribeToBoard(Guid boardId)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, boardId.ToString());
+            await Clients.Caller.SendAsync("Message", "Adicionado a uma sala com sucesso!");
+        }
+
+        public async Task RemoveFromBoard(Guid boardId)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, boardId.ToString());
+            await Clients.Caller.SendAsync("Message", "Removido de uma sala com sucesso!");
+        }
+    }
+}
---
+++ Pl4nn1ng-P0k3xx/Startup.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pl4nn1ng_P0k3xx.Hubs;
+
+namespace Pl4nn1ng_P0k3xx
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+            services.AddSignalR();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/build";
+            });
+
+            services.AddCors(options =>
+                options.AddPolicy("CorsPolicy",
+                    builder =>
+                        builder.AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithOrigins("http://localhost:3000")
+                        .AllowCredentials()));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHub<PlanningPokerHub>("/planningPoker");// Register Hub class
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer(npmScript: "start");
+                }
+            });
+
+            app.UseCors("CorsPolicy");
+        }
+    }
+}
