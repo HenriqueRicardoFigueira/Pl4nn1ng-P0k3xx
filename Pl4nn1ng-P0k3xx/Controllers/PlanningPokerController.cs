cead4815f3e84bc91ea757f393f492444d5bf49d
HenriqueRicardoFigueira <henrislip@gmail.com>
2021-05-16 20:18:18 -0300

    create routes and repository

---
+++ Pl4nn1ng-P0k3xx/Controllers/PlanningPokerController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Pl4nn1ng_P0k3xx.Data;
+using Pl4nn1ng_P0k3xx.Hubs;
+using System;
+using System.Threading.Tasks;
+
+namespace Pl4nn1ng_P0k3xx.Controllers
+{
+    [Route("planningPoker")]
+    [ApiController]
+    public class PlanningPokerController : ControllerBase
+    {
+        private readonly IPlanningRepository planningRepository;
+        private readonly IHubContext<PlanningPokerHub> hub;
+
+        public PlanningPokerController(IPlanningRepository planningRepository, IHubContext<PlanningPokerHub> hub)
+        {
+            this.planningRepository = planningRepository;
+            this.hub = hub;
+        }
+
+        [HttpPost("boards")]
+        public async Task<IActionResult> Post([FromBody] Planning planning)
+        {
+            var boardId = Guid.NewGuid();
+            planning.Id = boardId;
+
+            var isCreated = await planningRepository.AddBoard(planning);
+            if (isCreated)
+            {
+                return Ok(boardId);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost("boards/{boardId}")]
+        public async Task<IActionResult> UpdateUsersPoint(Guid boardId)
+        {
+            var isAdded = await planningRepository.ClearUsersPoint(boardId);
+            await hub.Clients.Group(boardId.ToString())
+                .SendAsync("UsersAdded", await planningRepository.GetUsersFromBoard(boardId));
+            if (isAdded)
+            {
+                return Ok(isAdded);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("boards/{boardId}/{state}")]
+        public async Task<IActionResult> UpdateUsersPointVisibility(Guid boardId, bool state)
+        {
+            var isToggled = await planningRepository.TogglePoints(boardId, state);
+            await hub.Clients.Group(boardId.ToString())
+                .SendAsync("UsersAdded", await planningRepository.GetUsersFromBoard(boardId));
+            if (isToggled)
+            {
+                return Ok(isToggled);
+            }
+            return NotFound();
+        }
+
+        [HttpPost("boards/{boardId}/users")]
+        public async Task<IActionResult> AddUser(Guid planningId, User user)
+        {
+            user.UserId = Guid.NewGuid();
+            var isAdded = await planningRepository.AddUserToBoard(planningId, user);
+            await hub.Clients.Group(planningId.ToString())
+                .SendAsync("UsersAdded", await planningRepository.GetUsersFromBoard(planningId));
+            if (isAdded)
+            {
+                return Ok(user.UserId);
+            }
+            return NotFound();
+        }
+
+        [HttpGet("boards/{boardId}/users")]
+        public async Task<IActionResult> GetUsers(Guid boardId)
+        {
+            var users = await planningRepository.GetUsersFromBoard(boardId);
+
+            return Ok(users);
+        }
+    }
+
+   
+
+}
---
+++ Pl4nn1ng-P0k3xx/PlanningRepository.cs
@@ -0,0 +1,85 @@
+﻿using Pl4nn1ng_P0k3xx.Data;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pl4nn1ng_P0k3xx
+{
+    public class PlanningRepository : IPlanningRepository
+    {
+        private readonly IDatabase database;
+
+        public PlanningRepository(ConnectionMultiplexer redis)
+        {
+            database = redis.GetDatabase();
+        }
+
+        public async Task<bool> AddBoard(Planning planning)
+        {
+            var isDone = await database.StringSetAsync(planning.Id.ToString(), JsonSerializer.Serialize(planning));
+
+            return isDone;
+        }
+
+        public async Task<bool> AddUserToBoard(Guid boardId, User user)
+        {
+            var data = await database.StringGetAsync(boardId.ToString());
+
+            if (data.IsNullOrEmpty)
+            {
+                return false;
+            }
+
+            var board = JsonSerializer.Deserialize<Planning>(data);
+            board.Users.Add(user);
+
+            return await AddBoard(board);
+        }
+
+        public async Task<List<User>> GetUsersFromBoard(Guid boardId)
+        {
+            var data = await database.StringGetAsync(boardId.ToString());
+
+            if (data.IsNullOrEmpty)
+            {
+                return new List<User>();
+            }
+
+            var board = JsonSerializer.Deserialize<Planning>(data);
+
+            return board.Users;
+        }
+
+        public async Task<bool> ClearUsersPoint(Guid boardId)
+        {
+            var data = await database.StringGetAsync(boardId.ToString());
+
+            if (data.IsNullOrEmpty)
+            {
+                return false;
+            }
+
+            var board = JsonSerializer.Deserialize<Planning>(data);
+            board.Users.ForEach(u => u.Point = 0);
+
+            return await AddBoard(board);
+        }
+
+        public async Task<bool> TogglePoints(Guid boardId, bool state)
+        {
+            var data = await database.StringGetAsync(boardId.ToString());
+
+            if (data.IsNullOrEmpty)
+            {
+                return false;
+            }
+
+            var board = JsonSerializer.Deserialize<Planning>(data);
+            board.Users.ForEach(u => u.ShowPoint = state);
+
+            return await AddBoard(board);
+        }
+    }
+}
